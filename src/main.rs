9d4cd5cbd6a79e28e72dba4209bef93d5ec4d70c
serezaiskin-cell <serezaiskin@gmail.com>
2026-08-04 15:55:16 +0000

    Debug: remove windows_subsystem, add panic hook to file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+// #![cfg_attr(target_os = "windows", windows_subsystem = "windows") temporarily removed for debug
 
 use anyhow::anyhow;
 use eframe::NativeOptions;
@@ -48,6 +48,11 @@ fn check_apo_available() -> bool {
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+    std::panic::set_hook(Box::new(|info| {
+        let msg = format!("PANIC: {}", info);
+        let _ = std::fs::write(std::env::temp_dir().join("hyper_panic.log"), msg);
+        eprintln!("{}", msg);
+    }));
 
     fn vlog(msg: &str) {
         let log_path = std::env::temp_dir().join("hyper_voice_debug.log");
